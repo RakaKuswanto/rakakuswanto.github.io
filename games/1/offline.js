d050fbda1667fb6aae76f8608b43b883e88b6a8e
Raka Kuswanto <raka.kuswanto@gmail.com>
2022-07-11 16:32:43 +0700

    upload portfolio adu gesek game

---
+++ games/1/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1657531223,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/obstacler-sheet0.png",
+		"images/obstacler-sheet1.png",
+		"images/point-sheet0.png",
+		"images/p1-sheet0.png",
+		"images/p2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tanah-sheet0.png",
+		"images/close-sheet0.png",
+		"images/cmdmulai-sheet0.png",
+		"images/pp-sheet0.png",
+		"media/sfx1.m4a",
+		"media/sfx1.ogg",
+		"media/skor.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
