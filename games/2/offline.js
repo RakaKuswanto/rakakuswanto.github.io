a232b88a01b0b97f0b24fc89e36fb3e9d724312c
Raka Kuswanto <raka.kuswanto@gmail.com>
2024-04-03 22:13:54 +0700

    add games twing twing

---
+++ games/2/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1712157137,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/obstacler-sheet0.png",
+		"images/obstacler-sheet1.png",
+		"images/point-sheet0.png",
+		"images/penghapus-sheet0.png",
+		"images/pp-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
