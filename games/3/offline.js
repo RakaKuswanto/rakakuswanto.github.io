06c8c5989c3b5739caa641f480e82b34164ddeb8
Raka Kuswanto <raka.kuswanto@gmail.com>
2024-04-12 00:41:17 +0700

    update dropbox

---
+++ games/3/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1712856640,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/pointleft-sheet0.png",
+		"images/gradientsuccess-sheet0.png",
+		"media/point.ogg",
+		"media/failed.ogg",
+		"media/success.ogg",
+		"media/swing.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
